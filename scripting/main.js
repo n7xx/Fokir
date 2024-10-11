6d922a7b6929296777f461037a5ec54b666d0713
Nashaat Fathy <133058089+n7xx@users.noreply.github.com>
2024-10-11 09:14:59 +0300

    Add files via upload

---
+++ scripting/main.js
@@ -0,0 +1,10 @@
+const options = {
+  strings: ["Designer", "Developer"],
+  typeSpeed: 50, 
+  backSpeed: 25,
+  loop: true, 
+  backDelay: 500, 
+  startDelay: 500,
+};
+
+const typed = new Typed(".typed-element", options);
